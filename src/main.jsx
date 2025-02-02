cf774c686440bd77a632d66ddc8ca0544ced51c9
David Ricapa Sanchez <albertxd40@gmail.com>
2025-02-01 21:45:07 -0500

    Tercera parte

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {PrimerComponente} from './PrimerComponente'
 import {Contador} from './Contador'
+import {Listado} from './Listado'
 import './styles.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Contador value={0} />
+    <Listado/>
   </StrictMode>,
 )
