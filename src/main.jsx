5b061d43c1d70b1175137f944233184067c20d6a
David Ricapa Sanchez <albertxd40@gmail.com>
2025-02-06 16:42:40 -0500

    Cuarta parte del proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {PrimerComponente} from './PrimerComponente'
 import {Contador} from './Contador'
-import {Listado} from './Listado'
+import {ListadoApp} from './ListadoApp'
 import './styles.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Listado/>
+    <ListadoApp/>
   </StrictMode>,
 )
